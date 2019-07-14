36fedca9aa537964566d9e5b1cc5950cb9e1b0fe
Alvian Pratama <xanvial@gmail.com>
2019-07-14 22:33:16 +0700

    Post link from specific user to tracker subreddit

--- devtracker.py
+++ devtracker.py
@@ -4,15 +4,16 @@ import praw
 reddit = praw.Reddit('bot1')
 
 ULreddit = reddit.subreddit("underlords")
+TrackerReddit = reddit.subreddit("UnderlordsDevTracker")
 
 print("vian test")
 #print(reddit.user.me())
 for comment in ULreddit.comments(limit=1000):
-    #print(comment.author.name)
     if(comment.author.name == "Xanvial"):
         print("----")
         print(comment.submission.title)
         print(comment.body)
         print(comment.permalink)
+        TrackerReddit.submit(comment.submission.title+" ["+comment.author.name+"]", url="https://www.reddit.com"+comment.permalink)
 
 print("vian test done")
