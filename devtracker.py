594a3abb4835a28ece6223f1b99e35bc9991a7e1
Alvian Pratama <xanvial@gmail.com>
2019-07-14 23:18:48 +0700

    Allow searching user from list
    Add variable to limit parsed data from specific date

--- devtracker.py
+++ devtracker.py
@@ -1,4 +1,5 @@
 import praw
+import time
 
 # Create the Reddit instance and log in
 reddit = praw.Reddit('bot1')
@@ -6,14 +7,28 @@ reddit = praw.Reddit('bot1')
 ULreddit = reddit.subreddit("underlords")
 TrackerReddit = reddit.subreddit("UnderlordsDevTracker")
 
+userlist = [
+    "Xanvial"
+    ]
+
+for u in userlist:
+    print("user:"+u)
+
 print("vian test")
 #print(reddit.user.me())
+TimeLastCheckComment = 1563097412.0
+tmpTime = 0
 for comment in ULreddit.comments(limit=1000):
-    if(comment.author.name == "Xanvial"):
+    if (tmpTime == 0):
+        tmpTime = comment.created_utc   # save the latest comment time
+    if(comment.created_utc > TimeLastCheckComment and comment.author.name in userlist):
         print("----")
         print(comment.submission.title)
         print(comment.body)
         print(comment.permalink)
+        print(comment.created_utc)
         TrackerReddit.submit(comment.submission.title+" ["+comment.author.name+"]", url="https://www.reddit.com"+comment.permalink)
 
+TimeLastCheckComment = tmpTime
+print(TimeLastCheckComment)
 print("vian test done")
