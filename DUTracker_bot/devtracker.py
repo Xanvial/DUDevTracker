edd5bc9534dd44267abcf784ea308bf984141493
Alvian Pratama <xanvial@gmail.com>
2019-07-14 23:48:05 +0700

    Create a new main function and create infinite loop to get the data

--- DUTracker_bot/devtracker.py
+++ DUTracker_bot/devtracker.py
@@ -2,38 +2,56 @@ import praw
 import time
 import os
 
-# Create the Reddit instance and log in
-reddit = praw.Reddit(username = os.environ["reddit_username"],
-                     password = os.environ["reddit_password"],
-                     client_id = os.environ["client_id"],
-                     client_secret = os.environ["client_secret"],
-                     user_agent = "Xanvial")
 
-ULreddit = reddit.subreddit("underlords")
-TrackerReddit = reddit.subreddit("UnderlordsDevTracker")
+def bot_login():
+	# Create the Reddit instance and log in
+	reddit = praw.Reddit(username = os.environ["reddit_username"],
+						 password = os.environ["reddit_password"],
+						 client_id = os.environ["client_id"],
+						 client_secret = os.environ["client_secret"],
+						 user_agent = "Xanvial")
+	return reddit
 
-userlist = [
-    "Xanvial"
-    ]
+def mainloop(reddit, TimeLastCheckComment):
+	ULreddit = reddit.subreddit("underlords")
+	TrackerReddit = reddit.subreddit("UnderlordsDevTracker")
 
-for u in userlist:
-    print("user:"+u)
+	userlist = [
+		"Xanvial"
+		]
 
-print("vian test")
-#print(reddit.user.me())
-TimeLastCheckComment = 1563097412.0
-tmpTime = 0
-for comment in ULreddit.comments(limit=1000):
-    if (tmpTime == 0):
-        tmpTime = comment.created_utc   # save the latest comment time
-    if(comment.created_utc > TimeLastCheckComment and comment.author.name in userlist):
-        print("----")
-        print(comment.submission.title)
-        print(comment.body)
-        print(comment.permalink)
-        print(comment.created_utc)
-        TrackerReddit.submit(comment.submission.title+" ["+comment.author.name+"]", url="https://www.reddit.com"+comment.permalink)
+	for u in userlist:
+		print("user:"+u)
 
-TimeLastCheckComment = tmpTime
-print(TimeLastCheckComment)
-print("vian test done")
+	print("vian test")
+	#print(reddit.user.me())
+	tmpTime = 0
+	for comment in ULreddit.comments(limit=1000):
+		if (tmpTime == 0):
+			tmpTime = comment.created_utc   # save the latest comment time
+		if(comment.created_utc > TimeLastCheckComment and comment.author.name in userlist):
+			print("----")
+			print(comment.submission.title)
+			print(comment.body)
+			print(comment.permalink)
+			print(comment.created_utc)
+			#TrackerReddit.submit(comment.submission.title+" ["+comment.author.name+"]", url="https://www.reddit.com"+comment.permalink)
+
+	print(tmpTime)
+	print("vian test done")
+	return tmpTime
+
+if __name__ == "__main__":
+	while True:
+		try:
+			print ("\nFetching comments..")
+			r = bot_login()
+			created_utc = 0
+			while True:
+				# Fetching all new comments that were created after created_utc time
+				created_utc = mainloop(r, created_utc)
+				time.sleep(5*60) # sleep 5 minutes
+
+		except Exception as e:
+			print (str(e.__class__.__name__) + ": " + str(e))
+			time.sleep(15)
